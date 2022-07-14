4fe3bac7ac359023f8950da5e89a13edd5fd4d45
Rahul Kakodkar <cacodcar@tamu.edu>
2022-07-14 13:37:33 -0500

    added source (citation) arg in classes

--- src/energiapy/components/location.py
+++ src/energiapy/components/location.py
@@ -144,25 +144,3 @@ class location:
         return self.name == other.name
 
 
-# class capacity_factor:
-#     """Object with varying capacity factor data
-#     """
-#     def __init__(self, locations: list, processes: list, scales: temporal_scale, varying_process_dict: dict, scheduling_scale_level: int, name:str = 'capacity_factor'):
-#         self.locations = locations
-#         self.processes = processes 
-#         self.varying_process_dict = varying_process_dict
-#         self.scales = scales.scale
-#         self.name = name
-#         self.capacity_factor = self.make_capacity_factor()
-        
-#     def __repr__(self):
-#         return self.name       
-    
-#     def make_capacity_factor(self):
-#         capacity_factor_dict = {location.name:{process.name: \
-#             {scale: self.varying_process_dict[location.name][process.name][scale] \
-#                 if process.name in self.varying_process_dict[location.name].keys() else 1 \
-#                 for scale in product(*self.scales.values())} \
-#                 for process in self.processes} for location in self.locations}
-        
-#         return capacity_factor_dict
