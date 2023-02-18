b193f225660d50c07b3e4ca14c0741d2688d2589
cacodcar <59067790+cacodcar@users.noreply.github.com>
2023-02-17 19:46:57 -0600

    math mode

--- src/energiapy/model/variables/binary.py
+++ src/energiapy/model/variables/binary.py
@@ -20,9 +20,10 @@ def generate_network_binary_vars(instance: ConcreteModel, scale_level:int= 0):
     scales_network_binary: Set of scales to define the network level
 
     .. math::
-        X_P (location, process, network_scale_index): Process binary, 1 if process is set up, 0 otherwise
 
-        X_S (location, process, network_scale_index): Storage binary variable, 1 if storage for resource is set up, 0 otherwise
+        X^{P}_{location, process, scale_{network}} \\textnormal{Process binary, 1 if process is set up, 0 otherwise}
+
+        X^{S}_{location, resource, scale_{network}}: \\textnormal{Storage binary variable, 1 if storage for resource is set up, 0 otherwise}
 
    Args:
         instance (ConcreteModel): pyomo instance
