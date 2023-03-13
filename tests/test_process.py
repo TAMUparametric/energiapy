41419b55bcd9fd8f6411a64d454dec14e6edc2cb
Rahul Kakodkar <cacodcar@tamu.edu>
2023-03-13 13:10:14 -0500

    multiconversion test

--- tests/test_process.py
+++ tests/test_process.py
@@ -4,14 +4,14 @@ from src.energiapy.components.process import Process, ProcessMode, CostDynamics
 def test_process_multiconv():
     Resource1 = Resource(name = 'resource1')
     Resource2 = Resource(name = 'resource2')    
-    Process_multi = Process(name = 'process_multi', conversion= {0: {Resource1: -1, Resource2:2}, 1:{Resource1: -1, Resource2:2.5}})
+    Process_multi = Process(name = 'process_multi', multiconversion= {0: {Resource1: -1, Resource2:2}, 1:{Resource1: -1, Resource2:2.5}})
     assert Process_multi.processmode == ProcessMode.multi
 
 
 def test_process_storage_mode():
     Resource1 = Resource(name = 'resource1')  
     Process_storage = Process(name = 'process_storage', storage= Resource1)
-    assert Process_storage.process_mode == ProcessMode.storage
+    assert Process_storage.processmode == ProcessMode.storage
     
 def test_process_storage_storemax(): 
     Resource1 = Resource(name = 'resource1')  
