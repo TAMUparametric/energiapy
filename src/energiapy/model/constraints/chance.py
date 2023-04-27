3d0d684de73b74137f5e7781eeaf509ff748fbde
Rahul Kakodkar <cacodcar@tamu.edu>
2023-04-27 08:00:54 -0500

    incomplete matrices

--- src/energiapy/model/constraints/chance.py
+++ src/energiapy/model/constraints/chance.py
@@ -16,71 +16,34 @@ from pyomo.environ import ConcreteModel, Constraint
 
 from ...utils.latex_utils import constraint_latex_render
 from ...utils.scale_utils import scale_list, scale_tuple
-from ...components.resource import Resource
-from ...components.location import Location
+from ...fitting.dist import fit
+from ...utils.data_utils import  min_max
 
-def constraint_demand(instance: ConcreteModel, guarantee: float,  demand_scale_level: int = 0, scheduling_scale_level: int = 0,loc_res_dict: dict = None, ) -> Constraint:
-    """Ensures that demand for resource is met at chosen temporal scale
 
-    Args:
-        instance (ConcreteModel): pyomo instance
-        demand_scale_level (int, optional): scale of demand decisions. Defaults to 0.
-        scheduling_scale_level (int, optional): scale of scheduling decisions. Defaults to 0.
-        demand_dict (dict, optional): demand at location. Defaults to {}.
+# def constraint_demand_chance(instance: ConcreteModel, demand_factor:dict, guarantee: float, dist:str = 'norm', demand_scale_level: int = 0, scheduling_scale_level: int = 0,loc_res_dict: dict = None, ) -> Constraint:
 
-    Returns:
-        Constraint: demand
-    """
-    # scales = scale_list(instance= instance, scale_levels = demand_scale_level+1)
-    scales = scale_list(instance=instance,
-                        scale_levels=len(instance.scales))
-    scale_iter = scale_tuple(
-        instance=instance, scale_levels=scheduling_scale_level + 1)
+#     # scales = scale_list(instance= instance, scale_levels = demand_scale_level+1)
+#     scales = scale_list(instance=instance,
+#                         scale_levels=len(instance.scales))
+#     scale_iter = scale_tuple(
+#         instance=instance, scale_levels=scheduling_scale_level + 1)
 
-    if loc_res_dict is None:
-        loc_res_dict = dict()
+#     if loc_res_dict is None:
+#         loc_res_dict = dict()
+    
+#     def norm_constant(p, mu, sigma):
+#         x = mu + erf(1 / sqrt(2) * p) * sigma * sqrt(2)
+#         return 1 / (sigma * sqrt(2 * pi)) * exp(-(x - mu)**2 / (2 * sigma**2))
 
-    def demand_rule(instance, location, resource, *scale_list):
+#         data = demand_factor[location].to_numpy()
+#         data = min_max(data)
+#         fit_summary = fit(data)
+#         mu = fit_summary.loc[dist]['loc']
+#         sigma = fit_summary.loc[dist]['scale']
+#         c= norm_constant(guarantee, mu, sigma)
 
-        if demand_factor[location] is not None:
-            if isinstance(demand_factor[location][list(demand_factor[location])[0]], (float, int)):
-                discharge = sum(instance.S[location, resource_, scale_list[:scheduling_scale_level + 1]] for
-                                resource_ in instance.resources_demand)
-            else:
-                discharge = sum(instance.S[location, resource, scale_] for scale_ in scale_iter if scale_[
-                    :scheduling_scale_level + 1] == scale_list)
 
-            if isinstance(demand, dict):
-                if resource in loc_res_dict[location]:
-                    demandtarget = demand[location][resource] * \
-                        demand_factor[location][resource][scale_list[:demand_scale_level + 1]]
-                else:
-                    demandtarget = 0
-            else:
-                if resource in loc_res_dict[location]:
-                    demandtarget = demand * \
-                        demand_factor[location][resource][scale_list[:demand_scale_level + 1]]
-                else:
-                    demandtarget = 0
-        else:
-            # TODO - doesn't meet demand in first timeperiod
-            discharge = sum(instance.S[location, resource, scale_] for scale_ in scale_iter if scale_[
-                :scheduling_scale_level + 1] == scale_list)
-
-            if isinstance(demand, dict):
-                demandtarget = demand[location][resource]
-            else:
-                demandtarget = demand
-
-        return discharge >= demandtarget
-
-    if len(instance.locations) > 1:
-        instance.constraint_demand = Constraint(
-            instance.sinks, instance.resources_demand, *scales, rule=demand_rule, doc='specific demand for resources')
-
-    else:
-        instance.constraint_demand = Constraint(
-            instance.locations, instance.resources_demand, *scales, rule=demand_rule,
-            doc='specific demand for resources')
-    constraint_latex_render(demand_rule)
-    return instance.constraint_demand
+#     def demand_chance_rule(instance, location, resource, *scale_list):
+     
+#     constraint_latex_render(demand_chance_rule)
+#     return instance.constraint_demand
