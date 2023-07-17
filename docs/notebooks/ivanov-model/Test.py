7392c3f0e8742244c41a4bde40b32d4043ccb8c9
shivam.vedant <shivam.vedant@tamu.edu>
2023-07-17 13:20:57 -0500

    Initial successful run

--- docs/notebooks/ivanov-model/Test.py
+++ docs/notebooks/ivanov-model/Test.py
@@ -30,13 +30,64 @@ scales = TemporalScale(discretization_list=[_exec_scenarios, _time_intervals])
 # Declare resources/commodities
 # ======================================================================================================================
 
-com1 = Resource(name='com1', cons_max=M, demand=True, sell=True, revenue= 10.00, price= 7.50,
+com1 = Resource(name='com1', cons_max=M, demand=True, sell=True, revenue= 100.00, price= 7.50,
                 block={'imp': 1, 'urg': 1}, label='Commodity 1', store_max=M)
 
 # ======================================================================================================================
 # Declare processes/storage capacities
 # ======================================================================================================================
-store50 = Process(name='store10', storage=com1, store_max=50, prod_max=M, conversion={com1: -1, com1: 1}, capex=5000,
-                  label="Storage capacity of 10 units")
 store20 = Process(name='store20', storage=com1, store_max=20, prod_max=M, conversion={com1: -1, com1: 1}, capex=2000,
                   label="Storage capacity of 20 units")
+store50 = Process(name='store50', storage=com1, store_max=50, prod_max=M, conversion={com1: -1, com1: 1}, capex=5000,
+                  label="Storage capacity of 50 units")
+
+# ======================================================================================================================
+# Declare locations/warehouses
+# ======================================================================================================================
+loc1 = Location(name='loc1', processes={store50}, label="Location 1", scales=scales, demand_scale_level=1,
+                capacity_scale_level=1, availability_scale_level=1)
+loc2 = Location(name='loc2', processes={store20}, label="Location 2", scales=scales, demand_scale_level=1,
+                capacity_scale_level=1, availability_scale_level=1)
+
+# ======================================================================================================================
+# Declare transport/trucks
+# ======================================================================================================================
+truck100 = Transport(name='truck100', resources=[com1], trans_max=100, label='Truck with maximum capacity of 100 units',
+                     trans_cost=0.1)
+
+transport_matrix = [
+    [[], [truck100]],  # sink: location 1
+    [[truck100], []],  # sink: location 2
+]
+
+distance_matrix = [
+    [0, 10],
+    [10, 0],
+]
+
+# ======================================================================================================================
+# Declare network
+# ======================================================================================================================
+locset = {loc1, loc2}
+
+sources = list(locset)
+sinks = list(locset)
+
+network = Network(name='Network', source_locations=sources, sink_locations=sinks, transport_matrix=transport_matrix,
+                  distance_matrix=distance_matrix)
+
+
+# ======================================================================================================================
+# Declare scenario
+# ======================================================================================================================
+demand_dict = {i: {com1: 100} for i in locset if i == loc2}
+scenario = Scenario(name='scenario', scales=scales, scheduling_scale_level=1, network_scale_level=0, expenditure_scale_level=0, purchase_scale_level=1,
+                    demand_scale_level=1, network=network, demand=demand_dict, label='scenario')
+
+
+problem = formulate(scenario=scenario, constraints={Constraints.COST, Constraints.TRANSPORT,
+                                                    Constraints.RESOURCE_BALANCE, Constraints.PRODUCTION,
+                                                    Constraints.INVENTORY, Constraints.RESOURCE_BALANCE},
+                    objective=Objective.COST)
+
+results = solve(scenario=scenario, instance=problem, solver='gurobi', name='LP')
