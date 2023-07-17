ccb482e81551beb6486e189db89963bb300e7eaf
shivam.vedant <shivam.vedant@tamu.edu>
2023-07-17 13:20:57 -0500

    Initial successful run

--- docs/notebooks/ivanov-model/IvanovCaseStudy.py
+++ docs/notebooks/ivanov-model/IvanovCaseStudy.py
@@ -33,7 +33,7 @@ from energiapy.components.network import Network
 from energiapy.components.scenario import Scenario
 from energiapy.components.result import Result
 from energiapy.model.formulate import formulate, Constraints, Objective
-from energiapy.plot import plot
+from energiapy.plot import plot_results, plot_scenario
 from energiapy.model.solve import solve
 
 # ======================================================================================================================
@@ -54,35 +54,43 @@ scales = TemporalScale(discretization_list=[_exec_scenarios, _time_intervals])
 # Declare resources/commodities
 # ======================================================================================================================
 
-com1 = Resource(name='com1', cons_max=M, demand=True, sell=True, block={'imp': 1, 'urg': 1}, label='Commodity 1')
+com_cons = Resource(name='com_cons', cons_max=M, block={'imp': 1, 'urg': 1}, price=7.50, label='Commodity consumed from outside the system')
+
+com1 = Resource(name='com1', demand=True, sell=True, block={'imp': 1, 'urg': 1}, price=7.50, revenue=10.00,
+                label='Commodity 1')
 
 # ======================================================================================================================
 # Declare processes/storage capacities
 # ======================================================================================================================
-store10 = Process(name='store10', storage=com1, store_max=10, prod_max=M, conversion={com1: -1, com1: 1}, capex=1000,
+procure = Process(name='procure', prod_max= M, conversion= {com_cons:-1, com1: 1}, capex = 0, vopex = 0, fopex = 0, label = 'Procure com1')
+
+store10 = Process(name='store10', storage=com1, store_max=10, prod_max=M, capex=1000,
+                  vopex=10,
                   label="Storage capacity of 10 units")
-store20 = Process(name='store20', storage=com1, store_max=20, prod_max=M, conversion={com1: -1, com1: 1}, capex=2000,
+store20 = Process(name='store20', storage=com1, store_max=20, prod_max=M, capex=2000,
+                  vopex=20,
                   label="Storage capacity of 20 units")
-store50 = Process(name='store50', storage=com1, store_max=50, prod_max=M, conversion={com1: -1, com1: 1}, capex=10000,
+store50 = Process(name='store50', storage=com1, store_max=50, prod_max=M, capex=10000,
+                  vopex=50,
                   label="Storage capacity of 50 units")
 
 # ======================================================================================================================
 # Declare locations/warehouses
 # ======================================================================================================================
-loc1 = Location(name='loc1', processes={store20}, label="Location 1", scales=scales, demand_scale_level=1,
-                capacity_scale_level=1, availability_scale_level=1)
+loc1 = Location(name='loc1', processes={procure, store20}, label="Location 1", scales=scales, demand_scale_level=1,
+                capacity_scale_level=0, availability_scale_level=0)
 loc2 = Location(name='loc2', processes={store20}, label="Location 2", scales=scales, demand_scale_level=1,
-                capacity_scale_level=1, availability_scale_level=1)
+                capacity_scale_level=0, availability_scale_level=0)
 loc3 = Location(name='loc3', processes={store20}, label="Location 3", scales=scales, demand_scale_level=1,
-                capacity_scale_level=1, availability_scale_level=1)
+                capacity_scale_level=0, availability_scale_level=0)
 loc4 = Location(name='loc4', processes={store50}, label="Location 4", scales=scales, demand_scale_level=1,
-                capacity_scale_level=1, availability_scale_level=1)
+                capacity_scale_level=0, availability_scale_level=0)
 loc5 = Location(name='loc5', processes={store50}, label="Location 5", scales=scales, demand_scale_level=1,
-                capacity_scale_level=1, availability_scale_level=1)
-loc6 = Location(name='loc6', processes={store10}, label="Location 6", scales=scales, demand_scale_level=1,
-                capacity_scale_level=1, availability_scale_level=1)
+                capacity_scale_level=0, availability_scale_level=0)
+loc6 = Location(name='loc6', processes={procure, store10}, label="Location 6", scales=scales, demand_scale_level=1,
+                capacity_scale_level=0, availability_scale_level=0)
 loc7 = Location(name='loc7', processes={store10}, label="Location 7", scales=scales, demand_scale_level=1,
-                capacity_scale_level=1, availability_scale_level=1)
+                capacity_scale_level=0, availability_scale_level=0)
 
 # ======================================================================================================================
 # Declare transport/trucks
@@ -123,17 +131,27 @@ distance_matrix = [
 # ======================================================================================================================
 locset = {loc1, loc2, loc3, loc4, loc5, loc6, loc7}
 
+
 sources = list(locset)
 sinks = list(locset)
 
 network = Network(name='Network', source_locations=sources, sink_locations=sinks, transport_matrix=transport_matrix,
                   distance_matrix=distance_matrix)
 
-demand_dict = {i: {com1: 100} for i in locset if i == loc5}
-scenario = Scenario(name='scenario', scales=scales, scheduling_scale_level=1, network_scale_level=0, expenditure_scale_level=0, purchase_scale_level=1,
+# demand_dict = {i: {com1: 100} for i in locset if i == loc5}
+demand_dict = {i : {com1: 100} if i == loc5 else {com1: 0} for i in locset}
+# demand_dict = {loc4: {com1: 0}, loc6: {com1: 0}, loc3: {com1: 0}, loc7: {com1: 0}, loc1: {com1: 0}, loc5: {com1: 100},
+#                loc2: {com1: 0}}
+
+scenario = Scenario(name='scenario', scales=scales, scheduling_scale_level=1, network_scale_level=0,
+                    purchase_scale_level=1,
                     demand_scale_level=1, network=network, demand=demand_dict, label='scenario')
 
 problem = formulate(scenario=scenario, constraints={Constraints.COST, Constraints.TRANSPORT,
                                                     Constraints.RESOURCE_BALANCE, Constraints.PRODUCTION,
-                                                    Constraints.INVENTORY, Constraints.NETWORK},
+                                                    Constraints.INVENTORY},
                     objective=Objective.COST)
+
+results = solve(scenario=scenario, instance=problem, solver='gurobi', name='MILP')
+
+problem.Trans_exp.pprint()
