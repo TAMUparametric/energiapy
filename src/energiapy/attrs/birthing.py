baf9e5c9b7cd3af58ce27aaf5591971bde564747
Rahul Kakodkar <cacodcar@tamu.edu>
2024-08-28 19:16:04 -0500

    birthing attrs

---
+++ src/energiapy/attrs/birthing.py
@@ -0,0 +1,19 @@
+"""Bound input attributes for Components
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from ..core.aliases.is_input import IsBoundInput
+
+
+@dataclass
+class StgBirthing:
+    """Bounds for Processes birthed in Storage"""
+
+    capacity_in: IsBoundInput = field(default=None)
+    capacity_out: IsBoundInput = field(default=None)
