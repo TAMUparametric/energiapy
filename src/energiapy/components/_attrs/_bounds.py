13ab59f47f11d3fe5745ade1567b2021cd358d8d
cacodcar <59067790+cacodcar@users.noreply.github.com>
2024-09-02 09:27:23 -0500

    plus, minus

--- src/energiapy/components/_attrs/_bounds.py
+++ src/energiapy/components/_attrs/_bounds.py
@@ -1,13 +1,27 @@
 """Bound Input attributes for all Defined Components
 """
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-
 from ...core.isalias.inps.isinp import IsBnd
 
 
+class _Ounds(ABC):
+    """Abstract Base Class for Bounds"""
+
+    @property
+    @abstractmethod
+    def plus(self):
+        """Positive sign in Balance"""
+
+    @property
+    @abstractmethod
+    def minus(self):
+        """Negative sign in Balance"""
+
+
 @dataclass
-class _PlyBounds:
+class _PlyB(_Ounds):
     """Bounds for Players
 
     Attributes:
@@ -19,9 +33,19 @@ class _PlyBounds:
     has: IsBnd = field(default=None)
     needs: IsBnd = field(default=None)
 
+    @property
+    def plus(self):
+        """Positive sign in Balance"""
+        return self.has
+
+    @property
+    def minus(self):
+        """Negative sign in Balance"""
+        return self.needs
+
 
 @dataclass
-class _CshBounds:
+class _CshBB(_Ounds):
     """Bounds for Cash
 
     Attributes:
@@ -33,9 +57,19 @@ class _CshBounds:
     spend: IsBnd = field(default=None)
     earn: IsBnd = field(default=None)
 
+    @property
+    def plus(self):
+        """Positive sign in Balance"""
+        return self.spend
+
+    @property
+    def minus(self):
+        """Negative sign in Balance"""
+        return self.earn
+
 
 @dataclass
-class _EmnBounds:
+class _EmnB(_Ounds):
     """Bounds for Emission
 
     Attributes:
@@ -44,10 +78,21 @@ class _EmnBounds:
     """
 
     emit: IsBnd = field(default=None)
+    abate: IsBnd = field(default=None)
+
+    @property
+    def plus(self):
+        """Positive sign in Balance"""
+        return self.emit
+
+    @property
+    def minus(self):
+        """Negative sign in Balance"""
+        return self.abate
 
 
 @dataclass
-class _UsdBounds:
+class _UsdB(_Ounds):
     """Bounds for Land and Material (Used)
 
     Attributes:
@@ -56,25 +101,48 @@ class _UsdBounds:
     """
 
     use: IsBnd = field(default=None)
+    dispose: IsBnd = field(default=None)
+
+    @property
+    def plus(self):
+        """Positive sign in Balance"""
+        return self.use
+
+    @property
+    def minus(self):
+        """Negative sign in Balance"""
+        return self.dispose
 
 
 @dataclass
-class _ResLocBounds:
+class _ResLocB(_Ounds):
     """Bounds for Resources at Locations"""
 
     buy: IsBnd = field(default=None)
     sell: IsBnd = field(default=None)
 
+    @property
+    def plus(self):
+        """Positive sign in Balance"""
+        return self.buy
+
+    @property
+    def minus(self):
+        """Negative sign in Balance"""
+        return self.sell
+
 
 @dataclass
-class _ResLnkBounds:
+class _ResLnkBB(_Ounds):
     """Bounds for Resources at Linkages"""
 
     ship: IsBnd = field(default=None)
+    # only for bi directional linkages
+    receive: IsBnd = field(default=None)
 
 
 @dataclass
-class _ResBounds(_ResLocBounds, _ResLnkBounds):
+class _ResBounds(_ResLocBB(_Ounds), _ResLnkBB(_Ounds)):
     """Bounds for Resources
 
     Attributes:
