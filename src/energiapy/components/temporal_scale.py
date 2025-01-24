3e0353e512f7f87808732b764cea5eea6caec8e7
Shiv <shivam.vedant@tamu.edu>
2025-01-24 17:25:51 -0600

    Git sync GERB

--- src/energiapy/components/temporal_scale.py
+++ src/energiapy/components/temporal_scale.py
@@ -13,6 +13,7 @@ __status__ = "Production"
 
 from dataclasses import dataclass
 from itertools import product
+from typing import Tuple
 
 
 @dataclass
@@ -35,7 +36,7 @@ class TemporalScale:
 
     """
     discretization_list: list 
-    start_zero: int = None 
+    start_zero: Tuple = None
 
     def __post_init__(self):
         """
@@ -47,9 +48,11 @@ class TemporalScale:
             name (str): the discretization list is the name.
             scale_levels (int): levels of the scale.
         """
-
-        self.scale_levels = len(self.discretization_list) 
-        self.scale = {i: list(range(self.discretization_list[i])) for i in range(self.scale_levels)}
+        self.scale_levels = len(self.discretization_list)
+        if self.start_zero and len(self.start_zero) != self.scale_levels:
+            raise ValueError("start zero input error")
+        else:
+            self.scale = {i: list(range(self.discretization_list[i])) for i in range(self.scale_levels)}
         self.list = list(range(len(self.discretization_list)))
         self.name = str(self.list)
 
@@ -62,7 +65,11 @@ class TemporalScale:
         Returns:
             List[tuple]: list of tuples with representing the scales
         """
-        return list(product(*[self.scale[i] for i in self.scale][:scale_level+1]))
+        if self.start_zero is None:
+            return list(product(*[self.scale[i] for i in self.scale][:scale_level+1]))
+        else:
+            # l = list(product(*[self.scale[i] for i in self.scale][:scale_level+1]))
+            return [j for j in list(product(*[self.scale[i] for i in self.scale][:scale_level+1])) if j >= self.start_zero[:scale_level+1]]
 
     def __repr__(self):
         return self.name
