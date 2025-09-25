e01cd54d1fa96d856952f2fa1555d48e516d639a
Kakodkar <cacodcar@tamu.edu>
2025-09-25 11:27:24 -0500

    commodity base class

--- src/energia/components/commodity/resource.py
+++ src/energia/components/commodity/resource.py
@@ -6,15 +6,12 @@
 """
 
 from dataclasses import dataclass
-from typing import Self
-
-from ...core.component import Component
-from ...modeling.parameters.conversion import Conversion
+from ._commodity import _Commodity
 from ...modeling.variables.default import Free, Inventory, Produce, Trade, Utilize
 
 
 @dataclass
-class Resource(Component, Trade, Produce, Utilize, Free, Inventory):
+class Resource(_Commodity, Trade, Produce, Utilize, Free, Inventory):
     """A resource, can be a material, chemical, energy, etc.
 
     Args:
@@ -31,8 +28,7 @@ class Resource(Component, Trade, Produce, Utilize, Free, Inventory):
     """
 
     def __post_init__(self):
-        # resources are components
-        Component.__post_init__(self)
+        _Commodity.__post_init__(self)
 
         # base resource, if any in conversion
         self.inv_of: Resource = None
@@ -40,21 +36,8 @@ class Resource(Component, Trade, Produce, Utilize, Free, Inventory):
         # resource in its stored form
         self.in_inv: list[Resource] = []
 
-        # list of conversions associated with the resource
-        self.conversions: list[Conversion] = []
-
         # flag indicates whether the resource is produced and expended insitu only
         self.insitu = False
-
-    @property
-    def conversion(self) -> dict[Conversion, int | float]:
-        """Conversion"""
-        # if no conversion is set, return 1.0
-        if not self.conversions:
-            return {self: 1.0}
-        # if there is a conversion, return its parameter in all tasks its
-        # associated with
-        return {task: task.conversion[self] for task in self.conversions}
 
     @property
     def demand(self):
@@ -65,61 +48,3 @@ class Resource(Component, Trade, Produce, Utilize, Free, Inventory):
     def price(self):
         """Cost of consume"""
         return self.consume[self.model.default_currency().spend]
-
-    def __setattr__(self, name, value):
-
-        if isinstance(value, Conversion):
-            # update conversions
-            self.conversions.append(value)
-
-        super().__setattr__(name, value)
-
-    def __mul__(self, other: int | float) -> Conversion:
-        # multiplying a number with a resources gives conversion
-        # math operations with conversions form the balance in tasks
-        conv = Conversion()
-        conv.conversion = {self: other}
-        return conv
-
-    def __rmul__(self, other: int | float) -> Conversion:
-        # reverse multiplication
-        return self * other
-
-    def __add__(self, other: Conversion) -> Conversion:
-        conv = Conversion()
-        if isinstance(other, Resource):
-            # if another resource is added, give it the parameter 1
-            conv.conversion = {self: 1, other: 1}
-            return conv
-
-        # if added with another conversion, updated the balance
-        conv.conversion = {self: 1, **other.conversion}
-        return conv
-
-    def __neg__(self) -> Conversion:
-        # just multiply by -1
-        return self * -1
-
-    def __sub__(self, other: Conversion | Self):
-        if isinstance(other, Resource):
-            # if another resource is subtracted
-            # give it the parameter -1
-            return self + -1 * other
-        if isinstance(other, Conversion):
-            # if another conversion is subtracted, update the balance
-            conv = Conversion()
-
-            conv.conversion = {
-                self: 1,
-                **{
-                    res: (
-                        -1 * par if isinstance(par, (int, float)) else [-i for i in par]
-                    )
-                    for res, par in other.conversion.items()
-                },
-            }
-            return conv
-
-    def __truediv__(self, other: int | float):
-        # treat division as multiplication by the inverse
-        return self * (1 / other)
