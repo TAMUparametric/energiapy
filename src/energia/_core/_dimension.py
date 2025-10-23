7d0a0e5ada067a8f2c16e225ad6335be228e6e44
Kakodkar <cacodcar@tamu.edu>
2025-10-23 13:17:35 -0500

    _Hash base class

--- src/energia/_core/_dimension.py
+++ src/energia/_core/_dimension.py
@@ -5,12 +5,14 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from .._core._hash import _Hash
+
 if TYPE_CHECKING:
     from ..represent.model import Model
 
 
 @dataclass
-class _Dimension:
+class _Dimension(_Hash):
     """
     Inherited by components that are a representation of
     some dimension of the model.
@@ -32,20 +34,3 @@ class _Dimension:
         # There is only one instance of these
         # so they take their own name
         self.name = rf"{self.__class__.__name__}({self.model})"
-
-    # -----------------------------------------------------
-    #                    Hashing
-    # -----------------------------------------------------
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return self.name
-
-    def __hash__(self):
-        return hash(self.name)
-
-    def __init_subclass__(cls):
-        cls.__repr__ = _Dimension.__repr__
-        cls.__hash__ = _Dimension.__hash__
